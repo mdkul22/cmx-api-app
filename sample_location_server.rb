bcea25e6734129708ef8226205e54dbc4dce58e9
mdkul22 <mdkul22@gmail.com>
2018-05-14 12:09:21 +0530

    syntax corrections

--- sample_location_server.rb
+++ sample_location_server.rb
@@ -244,9 +244,17 @@ post '/events' do
      floors = map['data']['apFloors'] == nil ? "" : map['data']['apFloors'].join
      logger.info "AP #{map['data']['apMac']} on #{map['data']['apFloors']}: #{c}"
      next if (seenEpoch == nil || seenEpoch == 0)  # This probe is useless, so ignore it
-     bleclient = BLEClient.first_or_create({:mac => name}, {:lat => lat,
-      :lng => lng, :seenString => seenString, :seenEpoch => seenEpoch,
-      :unc = loc['unc'], :x => x, :y => y, :floors => floors, :rssi => c['rssi']})
+     bleclient = BLEClient.first_or_create({:mac => name}, {
+       :lat => lat,
+       :lng => lng,
+       :seenString => seenString,
+       :seenEpoch => seenEpoch,
+       :unc = loc['unc'],
+       :x => x,
+       :y => y,
+       :floors => floors,
+       :rssi => c['rssi']
+       })
     if(true)
       bleclient.save
     end
